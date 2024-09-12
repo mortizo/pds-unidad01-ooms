aeb3fa65486c5b435cf793a62dd0befb6a7c10e4
morti <morti@172.16.2.119>
2024-09-12 15:05:41 -0500

    Mensaje 2

--- src/main/java/p65/poo/MainPoo.java
+++ src/main/java/p65/poo/MainPoo.java
@@ -15,7 +15,7 @@ public class MainPoo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo !!!");
+        System.out.println("Hola mundo!");
     }
     
 }

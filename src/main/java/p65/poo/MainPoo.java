0a81a7456256c50ca36225b7328ce60248fd3305
morti <morti@172.16.2.16>
2024-09-12 09:54:16 -0500

    Mensaje de hola mundo !!!

--- src/main/java/p65/poo/MainPoo.java
+++ src/main/java/p65/poo/MainPoo.java
@@ -15,6 +15,7 @@ public class MainPoo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo !!!");
     }
     
 }
